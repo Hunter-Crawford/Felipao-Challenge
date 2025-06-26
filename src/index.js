d84fd7f1324875afb4dee3bf069c77c47df88d0d
Hunter_Crawford <hunter.tjdss@gmail.com>
2025-06-26 11:52:51 -0300

    Base do desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-let name = "Hunter_Crawford"
-console.log("Welcome, you")
+//variáveis
+let heroname = "Hunter_Crawford"
+let herolevel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+let xprequired = [0, 1000, 2000, 5000, 7000, 8000, 9000, 10000]
+
+//operações
+
+
+//Output
+console.log("O Herói de nome" + heroname + "está no nível de" + herolevel)
